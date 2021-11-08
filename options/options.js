140a778f2f8c4688f59bf1d7b5798ff36390e519
Crowdbotics <team@crowdbotics.com>
2021-11-08 13:56:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "ssji_meeting_alert_31873",
+  url: "https://ssji_meeting_alert_31873.botics.co",
+  api: "https://ssji_meeting_alert_31873.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
